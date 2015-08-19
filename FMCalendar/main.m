5401774a1395aacb857a148cd4b7658cec826ced
fanmaoyu0871 <frank0871@163.com>
2015-08-19 23:00:51 +0800

    Initial Commit

---
+++ FMCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FMCalendar
+//
+//  Created by 范茂羽 on 15/8/19.
+//  Copyright (c) 2015年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FMCalendarTests/FMCalendarTests.m
@@ -0,0 +1,40 @@
+//
+//  FMCalendarTests.m
+//  FMCalendarTests
+//
+//  Created by 范茂羽 on 15/8/19.
+//  Copyright (c) 2015年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface FMCalendarTests : XCTestCase
+
+@end
+
+@implementation FMCalendarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
